27a2e8d5c7ea3aecae95147fbf042217f52c8779
영보 <gusqhdud96@gmail.com>
2021-10-25 15:41:33 +0900

    10.25 update,delete,insert,logout 기능 추가

--- WebProject/WebPractice01/source/BoardDAO.java
+++ WebProject/WebPractice01/source/BoardDAO.java
@@ -34,7 +34,7 @@ public class BoardDAO {
             where = "where "+ searchField + " like '%" + searchText+"%'";
          }
          
-         System.out.println("where: "+where);
+         System.out.println("where: " + where);
 
          String Condition_SQL = "select * from board "+ where +" order by seq desc";
 
@@ -61,5 +61,124 @@ public class BoardDAO {
       }
       
       return boardList;
-   }
+   } // ======================================= end getBoardList() =================================================
+   
+ //게시글 번호 조건에 맞는 해당 게시글만 검색하는 메소드
+   public BoardDO getBoard(BoardDO boardDO) {
+      System.out.println("==> getBoard() 처리됨");
+      
+      BoardDO board = null;
+      
+      try {
+         conn = JDBCUtil.getConnection();
+         
+         //[중요] 해당 게시글의 조회수(cnt)를 1 증가 시킨다.
+         String UPDATE_CNT = "update board set cnt=cnt+1 where seq=?";
+         pstmt = conn.prepareStatement(UPDATE_CNT);
+         pstmt.setInt(1, boardDO.getSeq());
+         pstmt.executeUpdate();	// DML 작업 시에는 executeUpdate로 호출
+         
+         //그런 다음 해당 게시글 가져오기
+         String BOARD_GET = "select * from board where seq=?";
+         pstmt = conn.prepareStatement(BOARD_GET);
+         pstmt.setInt(1, boardDO.getSeq());
+         rs = pstmt.executeQuery();
+         
+         if(rs.next()) {
+            board = new BoardDO();
+            
+            board.setSeq(rs.getInt("seq"));
+            board.setTitle(rs.getString("title"));
+            board.setWriter(rs.getString("writer"));
+            board.setContent(rs.getString("content"));
+            board.setRegdate(rs.getDate("regdate"));
+            board.setCnt(rs.getInt("cnt"));
+         }
+         
+         
+      } catch (Exception e) {
+         // TODO: handle exception
+         e.printStackTrace();
+      } finally {
+         JDBCUtil.close(rs, pstmt, conn);
+      }
+      
+      return board;
+   } // ======================================= end getBoard() =================================================
+   
+ //게시글 수정 처리 메소드
+   public int updateBoard(BoardDO boardDO) {
+      System.out.println("==> updateBoard() 처리됨!");
+      
+      int result=0;
+      try {
+         conn = JDBCUtil.getConnection();
+         
+         String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
+         
+         pstmt = conn.prepareStatement(BOARD_UPDATE);
+         pstmt.setString(1, boardDO.getTitle());
+         pstmt.setString(2, boardDO.getContent());
+         pstmt.setInt(3, boardDO.getSeq());
+         
+         result = pstmt.executeUpdate();
+         
+      } catch (Exception e) {
+         // TODO: handle exception
+      } finally {
+         
+      }
+      
+      System.out.println(result);
+      return result;
+   } // ======================================= end updateBoard() =================================================
+   
+   
+   public void deleteBoard(BoardDO boardDO) {
+	   System.out.println("==> deleteBoard() 처리됨!");
+	   
+	   try {
+		   conn = JDBCUtil.getConnection();
+		   
+		   String BOARD_DELETE = "delete from board where seq=?";
+		   
+		   pstmt = conn.prepareStatement(BOARD_DELETE);
+		   pstmt.setInt(1, boardDO.getSeq());
+		   
+		   pstmt.executeUpdate();
+	   }catch (Exception e) {
+		// TODO: handle exception
+		   e.printStackTrace();
+	   }finally {
+		JDBCUtil.close(pstmt, conn);
+	   }
+   } // ======================================= end deleteBoard() =================================================
+   
+   
+   public void insertBoard(BoardDO boardDO) {
+	      System.out.println("==> insertBoard() 처리됨!");
+	      
+	      int result=0;
+	      try {
+	         conn = JDBCUtil.getConnection();
+	         
+	         String BOARD_INSERT = "insert into board(seq,title,writer,content) values((select nvl(max(seq),0)+1 from board),?,?,?)";
+	         // 게시글이 없어도 seq 는 1부터 시작해서 1씩 증가
+	         
+	         pstmt = conn.prepareStatement(BOARD_INSERT);
+	         pstmt.setString(1, boardDO.getTitle());
+	         pstmt.setString(2, boardDO.getContent());
+	         pstmt.setString(3, boardDO.getWriter());
+	         
+	         pstmt.executeUpdate();
+	         
+	         
+	      } catch (Exception e) {
+	    	  e.printStackTrace();
+	         // TODO: handle exception
+	      } finally {
+	         JDBCUtil.close(pstmt, conn);
+	      }
+	   } // ======================================= end insertBoard() =================================================
+	   
 }
